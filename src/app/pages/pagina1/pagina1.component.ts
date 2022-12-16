1baa3fe5e17319daebc2cfffb9f16c0a72db9006
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-16 15:21:05 -0500

    ngOnDestroy

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -10,6 +10,7 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -28,16 +29,24 @@ export class Pagina1Component
     OnDestroy
 {
   nombre: string = 'f4biaan';
+  segundos: number= 0;
+  timerSubscription!: Subscription;
 
   // hook de ciclo de vida de Angular cuando sucede algo en el componente durante el ciclo de vida de un componente
   // se ejecuta cuando el componente se crea o se carga/construye el componente
 
   // ver en consola cuando se ejecuta cada uno de los hooks
   constructor() {
+    // lugar para poner codigo que necesitamos que se ejecute cuando se construye el componente
     console.log('Constructor');
   }
   ngOnInit(): void {
+    // el componente ya esta constrido
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe((i) => {
+      console.log(i);
+      this.segundos = i;
+    })
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges');
@@ -58,7 +67,10 @@ export class Pagina1Component
     console.log('ngAfterViewChecked');
   }
   ngOnDestroy(): void {
+    // se usa para terminar componente y evitar fugas de memoria en la aplicacion
     console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('timerSubscription terminado');
   }
 
   //
