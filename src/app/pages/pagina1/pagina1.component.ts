a4e00d1a002d1521eee0ddd677aa8e4d1e43f522
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-16 14:34:17 -0500

    life cycles implements

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Pagina1Component {
+export class Pagina1Component
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  // hook de ciclo de vida de Angular cuando sucede algo en el componente durante el ciclo de vida de un componente
+  // se ejecuta cuando el componente se crea o se carga/construye el componente
 
+  constructor() {
+    console.log('Constructor');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 }
